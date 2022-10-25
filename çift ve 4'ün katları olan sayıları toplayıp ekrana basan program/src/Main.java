ce91510dda4b29d1cdfd28a57dc8b4b8c9973cf6
MuraT110 <116304718+MuraT110@users.noreply.github.com>
2022-10-25 12:42:21 +0300

    Add files via upload

---
+++ çift ve 4'ün katları olan sayıları toplayıp ekrana basan program/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int s;
+        int total=0;
+
+
+        do {
+            Scanner inp = new Scanner(System.in);
+
+            System.out.print("LÜTFEN Bİ DEĞER GİRİNİZ:");
+            s = inp.nextInt();
+            if ((s % 2 == 0) && (s % 4 == 0)) {
+                total+=s;
+
+            }
+        } while (s%2==0);
+        System.out.println("TOPLAM:"+total);
+
+
+    }
+}
